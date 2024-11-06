e810af46406ca72c15633776c7a2f4c817f0ea8a
ss251 <ss251@uw.edu>
2024-11-06 22:06:25 +0530

    feat: update timeout

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb'
+    },
+    proxyTimeout: 60000, // 60 seconds in milliseconds
+  },
+  api: {
+    responseLimit: false,
+    bodyParser: {
+      sizeLimit: '2mb'
+    },
+    externalResolver: true
+  },
+  httpAgentOptions: {
+    keepAlive: true
+  }
 };
 
 export default nextConfig;
